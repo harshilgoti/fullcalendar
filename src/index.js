8fae81d710889b30fd1a99067e5bf498a9a8bfec
harshil goti <harshilgoti@gmail.com>
2021-04-05 18:02:56 +0530

    new

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DemoApp from "./DemoApp";
+import "./main.css";
+
+ReactDOM.render(
+  <DemoApp />,
+  // </AppContext.Provider>
+  document.getElementById("root")
+);
